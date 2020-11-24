40409dd9ca056dcb2e62c70d7fd851270e3400eb
Mohit Sharma <mohit.sharma@valuelabs.com>
2020-11-24 10:18:32 +0530

    latest

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Azure Pipeline Setup by pranab!!!');
+  res.send('Azure Pipeline Setup by vamsi!!!');
 });
 
 app.listen(config.port, () => {
